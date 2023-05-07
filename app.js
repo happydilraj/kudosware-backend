473e5353e88827810a9835ba9a46eec1b845bbb9
happydilraj <happydilraj1022@gmail.com>
2023-05-08 00:25:14 +0530

    commit

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.post("/register", async (req, resp) => {
         const user = new User(req.body);
         let result = await user.save();
         result = result.toObject();
-        resp.send("dilraj")
         if (result) {
             // delete result.password;
             resp.send(req.body);
